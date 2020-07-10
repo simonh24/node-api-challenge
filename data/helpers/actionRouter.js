cd4cfcd26ea38639d0bc2acb129eb2adb8ca14ae
Simon <simonh72498@gmail.com>
2020-07-10 11:25:58 -0400

    projectrouter completed

---
+++ data/helpers/actionRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const Actions = require("./actionModel");
+const Projects = require("./projectModel");
+
+const router = express.Router();
+// /api/actions
+router.get("/", (req, res) => {
+
+});
---
+++ data/helpers/projectRouter.js
@@ -0,0 +1,77 @@
+const express = require("express");
+
+const Projects = require("./projectModel");
+
+const router = express.Router();
+// /api/projects
+router.get("/", (req, res) => {
+    Projects.get()
+        .then(projs => {
+            res.status(200).json(projs);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "something went wrong" });
+        })
+});
+
+router.get("/:id", (req, res) => {
+    Projects.get(req.params.id)
+        .then(projs => {
+            if (projs.length === 0) {
+                res.status(404).json({ error: "id does not exist" });
+            } else {
+                res.status(200).json(projs[0]);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "something went wrong" });
+        })
+});
+
+router.post("/", (req, res) => {
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({ error: "missing name or description "});
+    } else {
+        Projects.insert({ ...req.body, completed: false })
+            .then(proj => {
+                res.status(201).json(proj);
+            })
+            .catch(err => {
+                res.status(500).json({ error: "something went wrong" });
+            })
+    }
+});
+
+router.put("/:id", (req, res) => {
+    Projects.update(req.params.id, req.body)
+        .then(proj => {
+            if (proj === null) {
+                res.status(404).json({ error: "id does not exist" });
+            } else {
+                res.status(200).json(proj);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "something went wrong" });
+        })
+});
+
+router.delete("/:id", (req, res) => {
+    Projects.get(req.params.id)
+        .then(proj => {
+            if (proj.length === 0) {
+                res.status(404).json({ error: "id does not exist" });
+            } else {
+                Projects.remove(req.params.id)
+                    .then(del => {
+                        res.status(202).json({ message: "project removed" });
+                    })
+                    .catch(err => {
+                        res.status(500).json({ error: "something went wrong." });
+                    })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "something went wrong" });
+        })
+});
